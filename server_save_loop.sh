707ed6b8414d86b6f72cb7e84f3d0ded4b69f431
mokshraj <mokshraj20@gmail.com>
2024-09-28 18:54:10 +0530

    Beta server push #8

--- server_save_loop.sh
+++ server_save_loop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while [ True ];
 do
-    git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${{ github.repository }}.git
-    git add .
-    git commit --allow-empty -m "server save"
-    git push origin master
-    sleep 300
+    nohup git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${{ github.repository }}.git
+    nohup git add .
+    nohup git commit --allow-empty -m "server save"
+    nohup git push origin master
+    nohup sleep 300
 done  
