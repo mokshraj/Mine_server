d16752ddec1f7117fc40414dbdb0bdd20ff6c7a3
mokshraj <mokshraj20@gmail.com>
2024-10-01 22:20:18 +0530

    rm master #2

--- server_save_loop.sh
+++ server_save_loop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm /home/runner/work/Mine_server/Mine_server/master.zip
 while [ True ];
 do
     nohup git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
