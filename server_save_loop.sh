7c26dd4264e5d3b3c5802f7542b9175710e73a81
mokshraj <mokshraj20@gmail.com>
2024-10-01 18:32:21 +0530

    loop test #2

--- server_save_loop.sh
+++ server_save_loop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while [ True ];
 do
-    nohup git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${{ github.repository }}.git
+    nohup git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
     nohup git add .
     nohup git commit --allow-empty -m "server save"
     nohup git push origin master
