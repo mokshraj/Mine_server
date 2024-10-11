3ec25e902c9e95036e04e0270423214e343b7bff
mokshraj <mokshraj20@gmail.com>
2024-10-12 02:15:07 +0530

    keep alive integration test

--- server_save_loop.sh
+++ server_save_loop.sh
@@ -3,8 +3,12 @@ rm /home/runner/work/Mine_server/Mine_server/master.zip
 while [ True ];
 do
     nohup git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+    nohup git status
     nohup git add .
+    nohup git status
     nohup git commit --allow-empty -m "server save"
+    nohup git status
     nohup git push origin master
+    nohup git status
     nohup sleep 300
 done  
