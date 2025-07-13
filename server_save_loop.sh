72a49a6c31be9980f64fefa64b56ec520a02e16a
mokshraj <mokshraj20@gmail.com>
2025-07-13 20:27:43 +0530

    keep_check

--- server_save_loop.sh
+++ server_save_loop.sh
@@ -2,6 +2,7 @@
 rm /home/runner/work/Mine_server/Mine_server/master.zip
 while [ True ];
 do
+    rm -f nohup.out
     nohup git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
     nohup git status
     nohup git add .
